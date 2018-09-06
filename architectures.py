4973e31ab5f7d067e581013c75cd2c191ff5c270
Elvir Crncevic <elvircrn@gmail.com>
2018-09-06 17:48:26 +0200

    Fix architecture output format and update unit tests

--- architectures.py
+++ architectures.py
@@ -47,7 +47,51 @@ def yolo_fast(inputs, is_training, dropout, scope=data.DEFAULT_SCOPE):
             return predictions
 
 
-def yolo_arch(inputs, is_training, dropout, scope=data.DEFAULT_SCOPE):
+def yolo_arch_fast(inputs, is_training, dropout, scope=data.DEFAULT_SCOPE):
+    with tf.variable_scope(scope):
+        with slim.arg_scope(
+                [slim.conv2d, slim.fully_connected],
+                weights_initializer=tf.contrib.layers.xavier_initializer(),
+                activation_fn=tf.nn.relu):
+            net = slim.repeat(inputs, 2, slim.conv2d, 1, [3, 3], padding='SAME', scope='conv1')
+            # / 2
+            net = slim.max_pool2d(net, 2, stride=2, scope='pool1')
+            net = slim.dropout(net, keep_prob=dropout, is_training=is_training, scope='dropout1')
+            # 320x180
+
+            net = slim.repeat(net, 3, slim.conv2d, 1, [3, 3], padding='SAME', scope='conv2')
+            # / 2
+            net = slim.max_pool2d(net, 2, stride=2, scope='pool2')
+            net = slim.dropout(net, keep_prob=dropout, is_training=is_training, scope='dropout2')
+            # 160x90
+
+            net = slim.repeat(net, 3, slim.conv2d, 1, [3, 3], padding='SAME', scope='conv3')
+            # / 2
+            net = slim.max_pool2d(net, 2, stride=2, scope='pool3')
+            net = slim.dropout(net, keep_prob=dropout, is_training=is_training, scope='dropout3')
+            # 80x45
+
+            net = slim.repeat(net, 3, slim.conv2d, 1, [3, 3], padding='SAME', scope='conv4')
+            # / 2
+            net = slim.max_pool2d(net, 2, stride=2, scope='pool4')
+            net = slim.dropout(net, keep_prob=dropout, is_training=is_training, scope='dropout4')
+            # 40x22
+
+            net = slim.repeat(net, 3, slim.conv2d, 1, [3, 3], padding='SAME', scope='conv5')
+            # / 2
+            net = slim.max_pool2d(net, 2, stride=2, scope='pool5')
+            net = slim.dropout(net, keep_prob=dropout, is_training=is_training, scope='dropout5')
+            # 20x11
+
+            net = slim.conv2d(net, 6, [4, 1], padding='VALID', scope='conv6')
+            # net = slim.dropout(net, keep_prob=dropout, is_training=is_training, scope='dropout6')
+            # 17x10
+
+            # net = slim.softmax(net, scope='sm1')
+
+    return net
+
+def yolo_arch_slow(inputs, is_training, dropout, scope=data.DEFAULT_SCOPE):
     with tf.variable_scope(scope):
         with slim.arg_scope(
                 [slim.conv2d, slim.fully_connected],
@@ -83,7 +127,7 @@ def yolo_arch(inputs, is_training, dropout, scope=data.DEFAULT_SCOPE):
             net = slim.dropout(net, keep_prob=dropout, is_training=is_training, scope='dropout5')
             # 20x11
 
-            net = slim.conv2d(net, 6, [4, 2], padding='VALID', scope='conv6')
+            net = slim.conv2d(net, 6, [4, 1], padding='VALID', scope='conv6')
             # net = slim.dropout(net, keep_prob=dropout, is_training=is_training, scope='dropout6')
             # 17x10
 
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
+
 import numpy as np
 import tensorflow as tf
+import architectures as arch
 
 import network
 
@@ -13,6 +15,14 @@ class TestLoss(unittest.TestCase):
         self.assertAlmostEqual(loss.numpy(), 5 + 2 * np.power(np.sqrt(2) - 1, 2) + 2)
 
         print("loss: {}", loss)
+
+    def test_arch(self):
+        labels = tf.constant(np.random.rand(64, 640, 320, 1), dtype=tf.float64)
+        predictions = arch.yolo_arch_fast(labels, False, 0.5)
+        truth = arch.yolo_arch_fast(labels, False, 0.5)
+        print(network.calculate_loss(truth, predictions).numpy())
+        print(network.calculate_loss(truth, predictions).numpy().shape)
+
 
 
 if __name__ == '__main__':
